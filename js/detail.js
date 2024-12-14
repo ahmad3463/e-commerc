b0ad30ee39eaa2e6f75d92f3e5fd76dc93b49b73
Ahmad khan <hadijack365@gmail.com>
2024-12-14 22:19:36 +0500

    detail page is complete and also start work on modal

--- js/detail.js
+++ js/detail.js
@@ -3,19 +3,17 @@ function QueryParam(value) {
     return urlValue.get(value); // Retrieves the query parameter value
 }
 
-const cardIn = QueryParam("product-1");
+const cardIn = QueryParam("product");
 
 const cardData = {
     card1: {
         image: "img/product-01.jpg",
         para: "Front Pocket Jumper",
         price: "$34.47",
-        description: "Here is the data about the item. This is a cool suit that is comfortable for women. People also like this. This is a summer collection. It is very useful. If you like, you can buy it here or visit our offline store for a discount. Thanks for your kindness!"
+        description: "The suit featured on this page is a perfect blend of style and comfort, designed for modern women who value both elegance and practicality. Crafted from high-quality, breathable fabric, it’s ideal for summer outings or casual gatherings. The timeless design, complete with a front pocket detail, adds a touch of sophistication while ensuring versatility for various occasions. Whether you’re dressing up or down, this suit is a wardrobe essential that guarantees comfort and style in every wear"
     }
 };
-console.log("Query Parameter Value:", cardIn);
-console.log(Object.keys(cardData));
-if (cardData[cardIn]) {
+    if (cardData[cardIn]) {
     document.getElementById("card-image").src = cardData[cardIn].image;
     document.getElementById("card-parah").innerText = cardData[cardIn].para;
     document.getElementById("card-price").innerText = cardData[cardIn].price;
@@ -23,3 +21,27 @@ if (cardData[cardIn]) {
 } else {
     document.querySelector(".container").innerHTML = "<p>card not found!</p>";
 }
+
+    // incremnet and decrement function 
+
+    const quantityInput = document.getElementById("quantity");
+    const incrementBtn = document.getElementById("increment");
+    const decrementBtn = document.getElementById("decrement");
+
+    incrementBtn.addEventListener('click' , () => {
+        let value = parseInt(quantityInput.value);
+        quantityInput.value = value + 1;
+    });
+
+    decrementBtn.addEventListener('click' , () => {
+        let value = parseInt(quantityInput.value);
+        if (value > 1){
+            quantityInput.value = value - 1;
+        } 
+    })
+
+//  goBack function 
+
+ function goBack(){
+    window.location.href = "index.html"
+ }
