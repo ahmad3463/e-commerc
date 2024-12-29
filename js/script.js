4513da3e84adb8a5fc13fd1d676e555007346d40
Ahmad khan <hadijack365@gmail.com>
2024-12-29 10:37:32 +0500

    fav funtion is completed

--- js/script.js
+++ js/script.js
@@ -9,6 +9,29 @@ window.addEventListener('scroll' , () => {
     }
 });
 
+    // add favbtn funtionalilty
+
+    const mainFav = document.getElementById("main-fav-btn");
+    const cards = document.querySelectorAll(".mt-2");
+    let mainCounter = 0;
+
+    
+    cards.forEach((card) => {
+        card.addEventListener('click' , () => {
+            const isActive = card.classList.contains('heart-active');
+            card.classList.toggle("heart-active");
+            // console.log("working")
+            if(isActive){
+                mainCounter -= 1;
+            }else{
+                mainCounter += 1;
+              }
+              mainFav.textContent = mainCounter;
+
+        });
+    });
+    
+
 // product section 
 
 document.getElementById("product-1").addEventListener('click' , function (){
